af3fe3fa0794c6a724b7082b717a669417e65d45
eeluwole1 <engrwolesax@gmail.com>
2025-11-01 19:40:05 -0500

    employee-role-db-structure

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import prisma from "./client";
+
+// this method will add default values to the database
+// IT WILL CLEAR THE DB WHEN INVOKED
+// see https://www.prisma.io/docs/orm/prisma-migrate/workflows/seeding
+async function main() {
+  await seedData();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+async function seedData() {}
